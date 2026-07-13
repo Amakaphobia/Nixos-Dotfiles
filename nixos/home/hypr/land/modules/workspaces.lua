c7cc1266d501c976fd3a78334b1102e2add87e24
amakaphobia <h.daive89@gmail.com>
2026-07-13 20:01:27 +0200

    notifs

--- nixos/home/hypr/land/modules/workspaces.lua
+++ nixos/home/hypr/land/modules/workspaces.lua
@@ -5,22 +5,29 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
+-- pavucontrol
+hl.window_rule({
+  name = "PavuControl Rules",
+  match = { class = "org.pulseaudio.pavucontrol" },
+  float = true,
+})
+
 -- Obsidian
 hl.window_rule({
-	name = "Obsidian Rules",
-	match = { class = "obsidian" },
+  name = "Obsidian Rules",
+  match = { class = "obsidian" },
 
-	workspace = "4",
-	opaque = true,
+  workspace = "4",
+  opaque = true,
 })
 
 -- Firefox
 hl.window_rule({
-	name = "Firefox Rules",
-	match = { class = "firefox" },
+  name = "Firefox Rules",
+  match = { class = "firefox" },
 
-	workspace = "3 silent",
-	opaque = true,
+  workspace = "3 silent",
+  opaque = true,
 })
 -- Workspace-Rules
 -- keep workspaces 1-4 peristent for term shell firefox and obsidian
@@ -31,23 +38,23 @@ hl.workspace_rule({ workspace = "4", default_name = "", persistent = true })
 
 -- default rules follow after:
 hl.window_rule({
-	name = "suppress-maximize-events",
-	match = { class = ".*" },
+  name = "suppress-maximize-events",
+  match = { class = ".*" },
 
-	suppress_event = "maximize",
+  suppress_event = "maximize",
 })
 
 hl.window_rule({
-	-- Fix some dragging issues with XWayland
-	name = "fix-xwayland-drags",
-	match = {
-		class = "^$",
-		title = "^$",
-		xwayland = true,
-		float = true,
-		fullscreen = false,
-		pin = false,
-	},
+  -- Fix some dragging issues with XWayland
+  name = "fix-xwayland-drags",
+  match = {
+    class = "^$",
+    title = "^$",
+    xwayland = true,
+    float = true,
+    fullscreen = false,
+    pin = false,
+  },
 
-	no_focus = true,
+  no_focus = true,
 })
