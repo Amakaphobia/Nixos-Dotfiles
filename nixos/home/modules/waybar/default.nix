4257321069076499af818708ff188ebfb4d1b45d
amakaphobia <h.daive89@gmail.com>
2026-07-15 16:39:59 +0200

    autogenerate color.css from theme

--- nixos/home/modules/waybar/default.nix
+++ nixos/home/modules/waybar/default.nix
@@ -1,8 +1,30 @@
 {
+  config,
   pkgs,
   inputs,
+  lib,
   ...
 }:
+let
+  theme = config.dave.theme.scheme;
+
+  colorsLib = import ../../lib/colors.nix {
+    inherit lib;
+  };
+
+  # @Mauve,Base
+  # and:
+  # @accent,background
+  colorCss = colorsLib.toGtkCss (theme.palette // theme.roles);
+  waybarConfig = pkgs.runCommand "waybar-config-${theme.name}" { } ''
+    mkdir -p "$out"
+
+    cp -R ${./config}/. "$out/"
+    chmod -R u+w "$out"
+
+    printf '%s\n' ${lib.escapeShellArg colorCss} > "$out/style/color.css"
+  '';
+in
 {
   programs.waybar = {
     enable = true;
@@ -11,5 +33,5 @@
     package = inputs.waybar.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 
-  xdg.configFile."waybar".source = ./config;
+  xdg.configFile."waybar".source = waybarConfig;
 }
