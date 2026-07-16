e88ebbefbd0a6a21a35b957e1592b5046b197641
amakaphobia <h.daive89@gmail.com>
2026-07-16 10:22:33 +0200

    decorations moved

--- nixos/home/modules/hypr/land/decorations.nix
+++ nixos/home/modules/hypr/land/decorations.nix
@@ -1,35 +1,37 @@
-{ config, lib, ...}:
+{ config, lib, ... }:
 let
   colorsLib = import ../../../lib/colors.nix {
     inherit lib;
   };
   colors = config.dave.theme.scheme.roles;
 
-  border1 = ( colorsLib.hyprRgba colors.accent1 "ff" );
-  border2 = ( colorsLib.hyprRgba colors.info1 "ff" );
-  borderInactive = ( colorsLib.hyprRgba colors.background2 "dd" );
+  border1 = (colorsLib.hyprRgba colors.accent1 "ff");
+  border2 = (colorsLib.hyprRgba colors.info1 "ff");
+  borderInactive = (colorsLib.hyprRgba colors.background2 "dd");
 in
 {
   wayland.windowManager.hyprland.settings = {
-    general = {
-      # Gaps
-      gaps_in = 3;
-      gaps_out = 8;
+    config = {
+      general = {
+        # Gaps
+        gaps_in = 3;
+        gaps_out = 8;
 
-      # border
+        # border
         border_size = 2;
-      col = {
-        active_border = {
-          colors = [
-            border2
-            border1
-          ];
-          angle = 90;
+        col = {
+          active_border = {
+            colors = [
+              border2
+              border1
+            ];
+            angle = 90;
+          };
+          inactive_border = borderInactive;
         };
-        inactive_border = borderInactive;
+        resize_on_border = false;
+        allow_tearing = false;
       };
-    resize_on_border = false;
-    allow_tearing = false;
     };
   };
 }
