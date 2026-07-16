ede134b3dc5ba183c8e7f1f35646fc27915f44df
amakaphobia <h.daive89@gmail.com>
2026-07-16 17:42:21 +0200

    font fix

--- nixos/home/themes/fonts.nix
+++ nixos/home/themes/fonts.nix
@@ -1,28 +1,34 @@
 { pkgs, ... }:
 {
-  # Fonts
+  # Install fonts into home manager profile
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-color-emoji
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+    liberation_ttf
+  ];
+
+  # default fonts
   fonts.fontconfig = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-color-emoji
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.symbols-only
-      liberation_ttf
-    ];
+    enable = true;
 
     defaultFonts = {
       sansSerif = [
         "Noto Sans"
         "Symbols Nerd Font"
       ];
+
       serif = [
         "Noto Serif"
         "Symbols Nerd Font"
       ];
+
       monospace = [
         "JetBrainsMono Nerd Font Mono"
         "Symbols Nerd Font Mono"
       ];
+
       emoji = [
         "Noto Color Emoji"
       ];
