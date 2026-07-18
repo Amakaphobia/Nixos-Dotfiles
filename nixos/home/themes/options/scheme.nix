99a6f5144ea3ebf2b5d93a44b925cbf757287136
amakaphobia <h.daive89@gmail.com>
2026-07-18 10:55:26 +0200

    json to nix

--- nixos/home/themes/options/scheme.nix
+++ nixos/home/themes/options/scheme.nix
@@ -5,43 +5,46 @@
 }:
 let
   inherit (lib) mkOption types;
+
   # 6 hexadecimals
   hexColor = types.strMatching "[0-9a-fA-F]{6}";
 
   theme = config.dave.theme;
 
+  inherit (../palettes/palette-layout.nix) terminalColorRoles semanticColorRoles;
+
   # List all semantic colors and their descriptions
 
-  semanticRoleDescriptions = {
-
-    # Neutral surfaces
-    background = "default background for applications";
-    surface = "default background for containers";
-    surfaceRaised = "visually elevated containers";
-    overlay = "Background for floating content such as menus, popovers and tooltips.";
-
-    # Foreground content
-    foregroundDisabled = "very muted forground tone";
-    foregroundMuted = "muted forground tone";
-    foreground = "default tone for text";
-
-    # Structure and focus
-    border = "default border tone";
-    focus = "default border tone for focused objects";
-
-    # Interactive colors
-    accent = "default accent color";
-    accentAlt = "alternative accent color";
-    accentForeground = "foreground accent color";
-    selection = "default selection color";
-    selectionForeground = "default selection foreground";
-
-    # Semantic status colors
-    success = "indicates success";
-    warning = "indicates a non critical problem";
-    error = "indicates a critical problem";
-    info = "indicates noteworthy information";
-  };
+  # semanticRoleDescriptions = {
+  #
+  #   # Neutral surfaces
+  #   background = "default background for applications";
+  #   surface = "default background for containers";
+  #   surfaceRaised = "visually elevated containers";
+  #   overlay = "Background for floating content such as menus, popovers and tooltips.";
+  #
+  #   # Foreground content
+  #   foregroundDisabled = "very muted forground tone";
+  #   foregroundMuted = "muted forground tone";
+  #   foreground = "default tone for text";
+  #
+  #   # Structure and focus
+  #   border = "default border tone";
+  #   focus = "default border tone for focused objects";
+  #
+  #   # Interactive colors
+  #   accent = "default accent color";
+  #   accentAlt = "alternative accent color";
+  #   accentForeground = "foreground accent color";
+  #   selection = "default selection color";
+  #   selectionForeground = "default selection foreground";
+  #
+  #   # Semantic status colors
+  #   success = "indicates success";
+  #   warning = "indicates a non critical problem";
+  #   error = "indicates a critical problem";
+  #   info = "indicates noteworthy information";
+  # };
 
   colorBySemanticRole = lib.mapAttrs (
     name: description:
@@ -49,26 +52,26 @@ let
       inherit description;
       type = hexColor;
     }
-  ) semanticRoleDescriptions;
+  ) semanticColorRoles;
 
-  terminalColorRoles = {
-    color0 = "black";
-    color1 = "red";
-    color2 = "green";
-    color3 = "yellow";
-    color4 = "blue";
-    color5 = "magenta";
-    color6 = "cyan";
-    color7 = "light-grey";
-    color8 = "dark-grey";
-    color9 = "bright-red";
-    color10 = "bright-green";
-    color11 = "bright-yellow";
-    color12 = "bright-blue";
-    color13 = "bright-magenta";
-    color14 = "bright-cyan";
-    color15 = "bright-white";
-  };
+  # terminalColorRoles = {
+  #   color0 = "black";
+  #   color1 = "red";
+  #   color2 = "green";
+  #   color3 = "yellow";
+  #   color4 = "blue";
+  #   color5 = "magenta";
+  #   color6 = "cyan";
+  #   color7 = "light-grey";
+  #   color8 = "dark-grey";
+  #   color9 = "bright-red";
+  #   color10 = "bright-green";
+  #   color11 = "bright-yellow";
+  #   color12 = "bright-blue";
+  #   color13 = "bright-magenta";
+  #   color14 = "bright-cyan";
+  #   color15 = "bright-white";
+  # };
   terminalColorOptions = lib.mapAttrs (
     name: colorName:
     mkOption {
